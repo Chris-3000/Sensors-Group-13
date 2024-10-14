76fe7177b2ec8a387b21259cd5853515a6d7a9ac
Chris-3000 <dogsonly734@gmail.com>
2024-10-14 11:44:58 +1100

    Empty AX=XB solver function

---
+++ AXXB_Solver.m
@@ -0,0 +1,9 @@
+function [X] = AXXB_Solver(A, B)
+    % AX = XB
+    % A = [R_A, t_A; zeros(1,3), 1]
+    % B = [R_B, t_B; zeros(1,3), 1]
+    % X = [R_X, t_X; zeros(1,3), 1]
+    % R_A * R_X = R_X * R_B
+    % R_A * t_X + t_A = R_X * t_B + t_X
+end
+
